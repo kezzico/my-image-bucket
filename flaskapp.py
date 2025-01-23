ffb73f57cade9fd50ef53091d54f54098f37bee6
Py <py@mailer.kezzi.co>
2025-01-23 05:56:25 +0000

    changes for wsgi and missing python-dotenv

--- flaskapp.py
+++ flaskapp.py
@@ -55,5 +55,5 @@ def upload_file():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5432)
